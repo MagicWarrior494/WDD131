fda43fa9cfc07ad71d7eb89a45636b12c7220d48
Matthew-Foushee <mfoushee@byui.edu>
2025-09-27 20:59:39 -0700

    Moved Toggle

--- mission/mission.js
+++ mission/mission.js
@@ -4,9 +4,11 @@ function changeTheme(){
 if(selectElement.value === "dark") {
         document.body.classList.add("dark");
         document.querySelector("img").src = "res/byui-logo_dark.png";
+        document.querySelector("img").alt = "Byui dark Logo";
     } else {
         document.body.classList.remove("dark");
         document.querySelector("img").src = "res/byui-logo_blue.webp";
+        document.querySelector("img").alt = "Byui light Logo";
     }
 }
 
